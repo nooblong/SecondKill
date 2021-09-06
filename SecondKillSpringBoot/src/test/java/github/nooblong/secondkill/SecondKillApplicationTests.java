779b6fd6c20c00169f938038b7f4470cdfbd3fc8
龙打野 <18128966990@163.com>
2021-09-06 18:25:47 +0800

    init

---
+++ SecondKillSpringBoot/src/test/java/github/nooblong/secondkill/SecondKillApplicationTests.java
@@ -0,0 +1,13 @@
+package github.nooblong.secondkill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondKillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
