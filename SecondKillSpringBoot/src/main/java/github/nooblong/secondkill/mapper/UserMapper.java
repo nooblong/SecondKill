f9592ca55741e4aefa265c18fead5afee39db286
龙打野 <18128966990@163.com>
2021-09-06 22:22:36 +0800

    regenerate and copy

---
+++ SecondKillSpringBoot/src/main/java/github/nooblong/secondkill/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package github.nooblong.secondkill.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import github.nooblong.secondkill.entity.User;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author nooblong
+ * @since 2021-09-06
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ SecondKillSpringBoot/src/main/java/github/nooblong/secondkill/service/IUserService.java
@@ -0,0 +1,16 @@
+package github.nooblong.secondkill.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import github.nooblong.secondkill.entity.User;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author nooblong
+ * @since 2021-09-06
+ */
+public interface IUserService extends IService<User> {
+
+}
