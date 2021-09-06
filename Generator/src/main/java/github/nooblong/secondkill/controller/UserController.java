2cdd8304f9da35988217f63760b9d95b850864d7
龙打野 <18128966990@163.com>
2021-09-06 22:16:04 +0800

    Generator

---
+++ Generator/src/main/java/github/nooblong/secondkill/controller/UserController.java
@@ -0,0 +1,20 @@
+package github.nooblong.secondkill.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author nooblong
+ * @since 2021-09-06
+ */
+@RestController
+@RequestMapping("/secondkill/user")
+public class UserController {
+
+}
---
+++ Generator/src/main/java/github/nooblong/secondkill/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package github.nooblong.secondkill.mapper;
+
+import github.nooblong.secondkill.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author nooblong
+ * @since 2021-09-06
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
